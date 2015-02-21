d2c7600cb443bd00e7e675b418adc4632b19c400
n8kolenberg <nkolenberg@gmail.com>
2015-02-21 16:11:50 +0000

    Add slideDown and slideUp method instead of fadeIn and fadeOut

--- myscript2.js
+++ myscript2.js
@@ -6,8 +6,8 @@ $('.answer').hide();
 $('h3.question').on('click', function(){
 	var $answer = $(this).next('.answer');
 	if ($answer.is(':hidden')) {
-		$answer.fadeIn(300);
-	} else {$answer.fadeOut(300);
+		$answer.slideDown();
+	} else {$answer.slideUp();
 
 	} //end if else
 
